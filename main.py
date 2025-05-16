596d086c5524542338f6dcca6d33ae09b3fe6200
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2025-05-16 18:11:33 -0400

    add sqlalchemy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from routes import users
 
 app = FastAPI()
 
@@ -6,3 +7,4 @@ app = FastAPI()
 async def root():
     return { "message": "Hello World" }
 
+app.include_router(users.router)
